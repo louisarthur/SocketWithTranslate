476cb997d750a6a9423c711108b34ccb1592f7fa
Louis Arthur <louisartur.53@gmail.com>
2019-09-25 19:46:06 -0300

    align of textarea functions

--- assets/js.js
+++ assets/js.js
@@ -1,34 +1,25 @@
-$(document).ready(function(){  
-    var socket = io.connect("http://localhost:3001");
-    var ready = false;
+$(document).ready(function() {
+  var socket = io.connect("http://localhost:3001");
+  var ready = false;
 
-    $("#submit").submit(function(e) {
-		e.preventDefault();
-		$("#nick").fadeOut();
-		$("#chat").fadeIn();
-		var name = $("#nickname").val();
-		//var time = new Date();
-		//$("#name").html(name);
-		//$("#time").html('First login: ' + time.getHours() + ':' + time.getMinutes());
+  $("#submit").submit(function(e) {
+    e.preventDefault();
+    $("#nick").fadeOut();
+    $("#chat").fadeIn();
+    var name = $("#nickname").val();
+    var password = $("#password").val();
+    ready = true;
+    socket.emit("join", name);
+  });
 
-		ready = true;
-		socket.emit("join", name);
+  $("#textarea").change(function() {
+    var text = $("#textarea").val();
+    var origem = $("#idiomaOrig").val();
+    var destino = $("#idiomaTran").val();
+    socket.emit("send", text, origem, destino);
+  });
 
-	});
-
-	$("#textarea").keypress(function(e){
-        if(e.which == 13) {
-			var text = $("#textarea").val();
-			var origem = $("#idiomaOrig").val();
-			var destino = $("#idiomaTran").val();		
-					socket.emit("send", text, origem, destino);
-			
-        }
-	});
-	
-	socket.on("translate", function(msg) {
-		$("#textTranslated").html(msg);
-	})
-
+  socket.on("translate", function(msg) {
+    $("#textTranslated").html(msg);
+  });
 });
-
