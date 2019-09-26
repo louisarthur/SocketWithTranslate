64a77d75f9903d116cadef8e56c2266e6db98ffa
Wellyson Gustavo Silva Oliveira <wellysongustavo@gmail.com>
2019-09-26 14:29:47 -0300

    Tratando exceção dos selects

--- assets/js.js
+++ assets/js.js
@@ -1,29 +1,51 @@
-$(document).ready(function() {
-  var socket = io.connect("http://localhost:3001");
-  var ready = false;
+$(document).ready(function(){  
+    var socket = io.connect("http://localhost:3000");
+    var ready = false;
 
-  $("#submit").submit(function(e) {
-    e.preventDefault();
-    $("#nick").fadeOut();
-    $("#chat").fadeIn();
-    var name = $("#nickname").val();
-    var password = $("#password").val();
-    ready = true;
-    socket.emit("join", name);
-  });
+    $("#submit").submit(function(e) {
+		e.preventDefault();
+		$("#nick").fadeOut();
+		$("#chat").fadeIn();
+		var name = $("#nickname").val();
+		var time = new Date();
+		$("#name").html(name);
+		$("#time").html('First login: ' + time.getHours() + ':' + time.getMinutes());
 
-  $("#trad").click(function() {
-    var text = $("#textarea").val();
-    var origem = $("#idiomaOrig").val();
-    var destino = $("#idiomaTran").val();
-    var encrypted = CryptoJS.AES.encrypt(text, "agua").toString();
-    socket.emit("send", encrypted, origem, destino);
-  });
+		ready = true;
+		socket.emit("join", name);
 
-  socket.on("translate", function(msg) {
-    var deseencrypt = CryptoJS.AES.decrypt(msg, "cuzcuz").toString(
-      CryptoJS.enc.Utf8
-    );
-    $("#textTranslated").html(deseencrypt);
-  });
+	});
+
+	$("#textarea").keypress(function(e){
+        if(e.which == 13) {
+        	var text = $("#textarea").val();
+        	$("#textarea").val('');
+        	var time = new Date();
+					$(".chat").append('<li class="self"><div class="msg"><span>' + $("#nickname").val() + ':</span><p>' + text + '</p><time>' + time.getHours() + ':' + time.getMinutes() + '</time></div></li>');
+					
+					socket.emit("send", text);
+					// automatically scroll down
+					document.getElementById('bottom').scrollIntoView();
+        }
+    });
+
+
+    socket.on("update", function(msg) {
+    	if (ready) {
+    		$('.chat').append('<li class="info">' + msg + '</li>')
+    	}
+    }); 
+
+    socket.on("chat", function(client,msg) {
+    	if (ready) {
+				var time = new Date();
+				$(".chat").append('<li class="field"><div class="msg"><span>' + client + ':</span><p>' + msg + '</p><time>' + time.getHours() + ':' + time.getMinutes() + '</time></div></li>');
+				
+    	}
+    });
+
+
+
+
 });
+
